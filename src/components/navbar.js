b0d877832a7d4457667a31314a4d9d5f0a6c70eb
priscilla-cassandra <priscilla-92@live.com>
2026-08-26 12:09:59 +0200

    feat: built mobile navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
 import "/styles/main.css";
+import { logout } from "../api/auth";
 
 //TO-DO: Import logout() function and add to logout nav link
 //TO-DO: Replace hardcoded credits, logout (and profile image) with real API once login flow is done!
 export function renderNavbar(isLoggedIn) {
   return `
-    <nav class="md:shadow-md">
+    <nav class="shadow-md">
 
         <!--DESKTOP NAVBAR -->
         <div class="hidden md:flex items-center justify-between px-10 py-7 2xl:max-w-3/4 mx-auto">
@@ -47,8 +48,39 @@ export function renderNavbar(isLoggedIn) {
         </div>
         
         <!-- MOBILE NAVBAR -->
-        <div>
+        <div class="md:hidden fixed bottom-0 inset-x-0 z-50 justify-around items-center bg-background py-5 px-3 shadow-[0_-4px_10px_-1px_rgba(0,0,0,0.2)]">
+            ${
+              isLoggedIn
+                ? `
+              <a href="/index.html" class="flex flex-col items-center text-xs text-text-color">
+                <span class="text-primary-blue"<i class="fa-solid fa-house-chimney"></i></span>
+                Forside
+              </a>
 
+              <a href="/html-pages/createlisting.html" class="flex flex-col items-center text-xs text-text-color">
+                <span class="text-primary-blue"><i class="fa-solid fa-circle-plus"></i></span>
+                Ny annonse
+              </a>
+
+              <a href="/html-pages/profile.html" class="flex flex-col items-center text-xs text-text-color">
+                <span class="text-primary-blue"><i class="fa-regular fa-circle-user"></i></span>
+                Min profil
+              </a>
+
+              <button id="logoutBtn" class="flex flex-col items-center text-xs text-text-color">
+                <span class="text-primary-blue"><i class="fa-solid fa-arrow-right-from-bracket"></i></span>
+                Logg ut
+              </button>
+              `
+                : `
+              <div class="flex items-center justify-center gap-4 w-full">
+                <a href="/html-pages/login.html" class="border bg-primary-blue text-white px-4 py-2 rounded-lg hover:bg-primary-blue-hover flex-1 text-center">Logg inn</a>
+                <span class="text-text-color">eller</span>  
+                <a href="/html-pages/register.html" class="border border-primary-blue text-primary-blue px-4 py-2 rounded-lg hover:bg-gray-100 flex-1 text-center">Registrer bruker</a>
+
+              </div>  
+              `
+            }
         </div>
     </nav>
     `;
