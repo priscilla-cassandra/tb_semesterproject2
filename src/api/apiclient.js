59bb541ee264f64af59551eb6cd138604bf731d4
priscilla-cassandra <priscilla-92@live.com>
2026-08-28 13:38:57 +0200

    feat: changed status codes

--- src/api/apiclient.js
+++ src/api/apiclient.js
@@ -4,24 +4,24 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 async function apiClient(endpoint, options = {}) {
   const { body, ...customOptions } = options; //Separates body from ...customOptions to check if a body exists (this decides the HTTP method), keeping customOptions separate
 
-  const apiKey = localStorage.getItem('apiKey');
-  const accessToken = localStorage.getItem('accessToken');
+  const apiKey = localStorage.getItem("apiKey");
+  const accessToken = localStorage.getItem("accessToken");
 
   const headers = {
-    'content-Type': 'application/json',
-    'X-Noroff-API-Key': API_KEY, //default
+    "content-Type": "application/json",
+    "X-Noroff-API-Key": API_KEY, //default
   };
 
   if (apiKey) {
-    headers['X-Noroff-API-Key'] = apiKey; //override default with the users own key
+    headers["X-Noroff-API-Key"] = apiKey; //override default with the users own key
   }
 
   if (accessToken) {
-    headers['Authorization'] = `Bearer ${accessToken}`; //If user is logged in, attach accessToken to the headers
+    headers["Authorization"] = `Bearer ${accessToken}`; //If user is logged in, attach accessToken to the headers
   }
 
   const config = {
-    method: body ? 'POST' : 'GET', //If there is a body, it's a POST request, if not it's a GET request
+    method: body ? "POST" : "GET", //If there is a body, it's a POST request, if not it's a GET request
     ...customOptions, //customOptions will override the default method
     headers: {
       //spread default headers first, then custom headers so that the custom overrides the default
@@ -37,14 +37,16 @@ async function apiClient(endpoint, options = {}) {
   try {
     const response = await fetch(BASE_URL + endpoint, config); //The actual network call
     if (!response.ok) {
-      let message = 'An API error occured'; //fallback, in case nothing else works
+      let message = "An API error occured"; //fallback, in case nothing else works
       try {
         const errorData = await response.json();
         message = errorData.errors?.[0]?.message || message;
       } catch {
         message = response.statusText || message;
       }
-      throw new Error(message);
+      const error = new Error(message);
+      error.status = response.status;
+      throw error;
     }
 
     if (response.status === 204) {
@@ -54,7 +56,7 @@ async function apiClient(endpoint, options = {}) {
 
     return await response.json(); //If there is a body, parse it as JSON and return it to whoever called the API client
   } catch (error) {
-    console.error('API Client Error', error);
+    console.error("API Client Error", error);
     throw error;
   }
 }
@@ -62,5 +64,5 @@ async function apiClient(endpoint, options = {}) {
 export const get = (endpoint) => apiClient(endpoint);
 export const post = (endpoint, body) => apiClient(endpoint, { body });
 export const put = (endpoint, body) =>
-  apiClient(endpoint, { method: 'PUT', body });
-export const del = (endpoint) => apiClient(endpoint, { method: 'DELETE' });
+  apiClient(endpoint, { method: "PUT", body });
+export const del = (endpoint) => apiClient(endpoint, { method: "DELETE" });
