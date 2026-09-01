4660d8ce50d3498c715d1e751722acff16d6a6cf
priscilla-cassandra <priscilla-92@live.com>
2026-09-01 11:04:10 +0200

    fix: fixed typo

--- src/components/bidform.js
+++ src/components/bidform.js
@@ -3,7 +3,7 @@ import "/styles/main.css";
 export function renderBidForm(listing) {
   return `
         <article>
-            <input type="number" id="bid-input" aria-label="Bud min="1" step="1" placeholder="Legg inn bud her" class="w-full rounded border py-2 text-center"/>
+            <input type="number" id="bid-input" aria-label="Bud" min="1" step="1" placeholder="Legg inn bud her" class="w-full rounded border py-2 text-center"/>
             <button id="bidBtn" type="button" class="bg-cta text-white w-full rounded py-2">Gi bud</button>
             <div id="credits-mobile" class="md:hidden"></div>
         </article>
