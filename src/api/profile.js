62942c857e8b8599be74b8d07f2d542beeda6629
priscilla-cassandra <priscilla-92@live.com>
2026-09-02 15:13:45 +0200

    feat: API calls for gertBidsByProfile and getListingsByProfile

--- src/api/profile.js
+++ src/api/profile.js
@@ -4,3 +4,13 @@ export async function getProfile(name) {
   const { data } = await get(`/auction/profiles/${name}`);
   return data;
 }
+
+export async function getBidsByProfile(name) {
+  const { data } = await get(`/auction/profiles/${name}/bids?_listings=true`);
+  return data;
+}
+
+export async function getListingsByProfile(name) {
+  const { data } = await get(`/auction/profiles/${name}/listings?_bids=true`);
+  return data;
+}
