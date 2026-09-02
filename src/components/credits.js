e10f7ba6310e2d4ec88c8bab030c5370d484ae99
priscilla-cassandra <priscilla-92@live.com>
2026-09-02 12:31:47 +0200

    feat: render function for credits component on desktop and mobile

---
+++ src/components/credits.js
@@ -0,0 +1,17 @@
+export function renderCreditsDesktop(credits) {
+  return `
+        <section aria-labelledby="credits-heading" class="flex items-center gap-2">
+            <i class="fa-solid fa-coins fa-lg text-primary-blue"></i>
+            <p class="text-lg">${credits}</p>
+        </section>
+    `;
+}
+
+export function renderCreditsMobile(credits) {
+  return `
+        <section aria-labelledby="credits-heading" class=" text-md rounded-lg text-center flex gap-2 bg-linear-to-r from-primary-blue via-gradient-blue-middle to gradient-blue py-2 px-4">
+            <i class="fa-solid fa-coins text-white"></i>
+            <p class="text-white">${credits}</p>
+        </section>
+    `;
+}
