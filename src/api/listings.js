312ab3b1ac1c2c83be5a456f931931d1aa44e3cc
priscilla-cassandra <priscilla-92@live.com>
2026-09-04 14:41:31 +0200

    feat: async function for editListing and deleteListing

--- src/api/listings.js
+++ src/api/listings.js
@@ -1,5 +1,7 @@
 import { get } from "../api/apiclient";
 import { post } from "../api/apiclient";
+import { put } from "../api/apiclient";
+import { del } from "../api/apiclient";
 
 export async function getListings() {
   const { data } = await get("/auction/listings?sort=created&sordOrder=desc");
@@ -7,11 +9,20 @@ export async function getListings() {
 }
 
 export async function getSingleListing(id) {
-  const { data } = await get(`/auction/listings/${id}?_bids=true`);
+  const { data } = await get(`/auction/listings/${id}?_bids=true_seller=true`);
   return data;
 }
 
 export async function postNewListing(requestBody) {
   const { data } = await post(`auction/listings`, requestBody);
   return data;
+}
+
+export async function editListing() {
+  const { data } = await put(`/auction/listings/${id}`);
+  return data;
+}
+
+export async function deleteListing(id) {
+  await del(`/auction/listings/${id}`);
 }
