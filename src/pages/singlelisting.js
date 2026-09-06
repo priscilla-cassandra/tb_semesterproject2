0265e27474578fcb4d07109706db5ec739362152
priscilla-cassandra <priscilla-92@live.com>
2026-09-06 15:15:31 +0200

    feat: added chekpoint in single listings to check is the logged in user is the owner

--- src/pages/singlelisting.js
+++ src/pages/singlelisting.js
@@ -2,10 +2,13 @@ import { getSingleListing } from "../api/listings";
 import { renderBidForm, placeBid } from "../components/bidform";
 import { renderBidHistory } from "../components/bidhistory";
 import { renderSingleListing } from "../components/listingcard";
+import { isOwner } from "../api/auth";
+import { renderListingActions } from "../components/listingactions";
 
 const container = document.getElementById("single-listing");
 
 function renderSingleListingPage(listing) {
+  const owner = isOwner(listing);
   return `
         <div class="md:w-1/2">
             ${renderSingleListing(listing)}
@@ -14,7 +17,7 @@ function renderSingleListingPage(listing) {
             <div id="bid-history-container">
                ${renderBidHistory(listing.bids)} 
             </div>
-            ${renderBidForm(listing)}
+            ${owner ? renderListingActions(listing) : renderBidForm(listing)}
         </div>
     `;
 }
@@ -26,7 +29,10 @@ async function initPageLoad() {
   const listing = await getSingleListing(id);
   container.innerHTML = renderSingleListingPage(listing);
 
-  placeBid(listing);
+  //If the logged in user is NOT the owner of the listing, call placeBid()
+  if (!isOwner(listing)) {
+    placeBid(listing);
+  }
 }
 
 initPageLoad();
