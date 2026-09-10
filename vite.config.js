b07780f49711ddcfb33e253382b15e7621ff7211
priscilla-cassandra <priscilla-92@live.com>
2026-09-10 15:21:20 +0200

    fix: contact-page gave a 404 error on the deployed page. Had forgotten to add it to my vite.config rollup

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,32 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(import.meta.dirname, 'index.html'),
-        login: resolve(import.meta.dirname, 'html-pages/login.html'),
-        register: resolve(import.meta.dirname, 'html-pages/register.html'),
-        profile: resolve(import.meta.dirname, 'html-pages/profile.html'),
+        main: resolve(import.meta.dirname, "index.html"),
+        login: resolve(import.meta.dirname, "html-pages/login.html"),
+        register: resolve(import.meta.dirname, "html-pages/register.html"),
+        profile: resolve(import.meta.dirname, "html-pages/profile.html"),
+        contact: resolve(import.meta.dirname, "html-pages/contact.html"),
         editprofile: resolve(
           import.meta.dirname,
-          'html-pages/editprofile.html',
+          "html-pages/editprofile.html",
         ),
         createlisting: resolve(
           import.meta.dirname,
-          'html-pages/createlisting.html',
+          "html-pages/createlisting.html",
         ),
         editlisting: resolve(
           import.meta.dirname,
-          'html-pages/editlisting.html',
+          "html-pages/editlisting.html",
         ),
         singlelisting: resolve(
           import.meta.dirname,
-          'html-pages/singlelisting.html',
+          "html-pages/singlelisting.html",
         ),
       },
     },
