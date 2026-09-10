9790914737cc1b572c40c7304153dc10fa404587
priscilla-cassandra <priscilla-92@live.com>
2026-09-10 10:26:32 +0200

    style: added spacing

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -40,7 +40,7 @@ export function renderProfile(profile) {
                     <button type="button" id="logout-button" class="flex-1 bg-white border border-primary-blue rounded-lg text-primary-blue py-2 min-w-[160px] font-semibold">Logg ut</button>
                 </div>
             </section>
-            <section class="flex flex-col gap-3">
+            <section class="flex flex-col gap-3 mb-6">
                 <h2 class="text-lg md:text-2xl font-semibold">Mine annonser</h2>
                 ${listings.map(renderProfileListingCard).join("")}
             </section>
